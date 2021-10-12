1e01adcc1260c5eba9e987dae379738fb6f16d8a
kimjunsoo <rlawnstn01023@naver.com>
2021-10-12 10:28:34 +0900

    Initial Commit

---
+++ Moving_Text_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moving_Text_Demo
+//
+//  Created by 준수김 on 2021/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
