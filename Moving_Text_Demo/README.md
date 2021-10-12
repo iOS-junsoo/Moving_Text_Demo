c8a47934618cd43eef94a566dac9ffadd69fa4c7
kimjunsoo <rlawnstn01023@naver.com>
2021-10-12 10:51:33 +0900

    움직이는 텍스트

---
+++ Moving_Text_Demo/README.md
@@ -0,0 +1,2 @@
+#  https://ozofweird.tistory.com/entry/iOS-%EC%99%B8%EB%B6%80-%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%9F%AC%EB%A6%AC-2
+
--- Moving_Text_Demo/ViewController.swift
+++ Moving_Text_Demo/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import MarqueeLabel
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var marqueeLabel: MarqueeLabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.marqueeLabel.type = .continuous
+        self.marqueeLabel.speed = .duration(15)
+        self.marqueeLabel.animationCurve = .easeInOut
+        self.marqueeLabel.fadeLength = 10.0
+        self.marqueeLabel.leadingBuffer = 30.0
+        
+        let strings = ["오늘의 총 확진자 수는 165명 입니다. 전일 대비 확진자 수 가 29명 증가하였으니 안전한 하루 보내시길 바랍니다."]
+        
+        self.marqueeLabel.text = strings[Int(arc4random_uniform(UInt32(strings.count)))]
     }
-
-
+    
 }
 
